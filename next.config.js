4c1d596475ef5114ac2b487e58338cf66bd0f655
smartchainark <smartchainark@example.com>
2025-07-05 10:57:24 +0800

    更新 next.config.js 文件以添加允许的开发源配置，修改 package.json 中的开发脚本以简化启动命令。

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
+  // 其他配置...
+  allowedDevOrigins: ['192.168.31.173'], // 允许的开发源
 };
 
 module.exports = nextConfig;
